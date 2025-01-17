4ec280b7dc4a4c2392d518830af7691e5bb3f8b2
LG33 <louisgaillard33@gmail.com>
2025-01-17 14:01:18 +0000

    changed default colors in config dist

--- src/config.dist.php
+++ src/config.dist.php
@@ -656,19 +656,19 @@ namespace Paheko;
  * Couleur primaire de l'interface admin par défaut
  * (peut être personnalisée dans la configuration)
  *
- * Défaut : #20787a
+ * Défaut : #faa130
  * @var string
  */
 
-//const ADMIN_COLOR1 = '#20787a';
+//const ADMIN_COLOR1 = '#faa130';
 
 /**
  * Couleur secondaire de l'interface admin
- * Défaut : #85b9ba
+ * Défaut : #ffce8a
  * @var string
  */
 
-//const ADMIN_COLOR2 = '#85b9ba';
+//const ADMIN_COLOR2 = '#ffce8a';
 
 /**
  * Image de fond par défaut de l'interface admin
