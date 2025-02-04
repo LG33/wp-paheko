d627b8c35a3e98034f4560144299f1ca34c1acf4
LG33 <louisgaillard33@gmail.com>
2025-02-04 18:07:27 +0100

    removed data folder

--- src/data/plugins/helloasso_checkout/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Paheko;
-
-$plugin->unregisterSignal('cron');
-
-Extensions::toggle('helloasso_checkout_snippets', false);
-
-Utils::deleteRecursive(ROOT . '/modules/helloasso_checkout_snippets');
